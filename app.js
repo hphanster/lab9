e5cc77970cdf806e9d9d0014e6b0fde3e53b5609
leonphan30 <leonphan30@gmail.com>
2021-03-12 19:01:35 -0800

    first commit with new reepo and .js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let maxiq = 350;
+let brainlobe = frontal;
+
+const brainlobes_perside = 4;
+const brainlobes = true;
+
+if (maxiq === brainlobes) {
+    console.log ("They match")
+} else {
+    console.log("No luck")
+}
+for (letter of brainlobe) {
+    console.log(letter)
+}
